81ffeda46c0fab07f49a18138e983102eac84ce2
tBaronDar <thedarelis@gmail.com>
2025-11-01 15:19:29 +0200

    introduced layout

--- src/config/create-router.jsx
+++ src/config/create-router.jsx
@@ -1,22 +1,23 @@
 import { createBrowserRouter, redirect } from 'react-router-dom';
 
 import HomePage from '@/home/page';
+import Layout from '@/shared/layout';
 
 export const createRouter = () => {
   return createBrowserRouter([
     {
       path: '/',
-      element: <HomePage />,
-    },
-    {
-      path: '/dashboard',
-      element: (
-        <div>
-          <h1>Dashboard Page </h1>
-          <p>Den ehei tpt edo</p>
-          <a href='/'>Tipota</a>
-        </div>
-      ),
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          element: <HomePage />,
+        },
+        {
+          path: '/dashboard',
+          element: <div>Dashboard Page</div>,
+        },
+      ],
     },
     {
       path: '*',
