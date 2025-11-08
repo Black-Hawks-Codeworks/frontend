849c93ec127e7e6924a937866808829e7bc0bf5c
IliasMaz <162236667+IliasMaz@users.noreply.github.com>
2025-11-08 23:19:12 +0200

    app bar change route from home page to layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import { RouterProvider } from 'react-router-dom';
 import { createRouter } from '@/config/create-router';
-import AppBar from './components/AppBar/AppBar';
 
 function App() {
-  <AppBar />;
   return (
-    <>
-      <AppBar />
+  
       <RouterProvider router={createRouter()} />
-    </>
+
   );
 }
 
