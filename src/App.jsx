17a3ecdbffb1114ae5dee4c322dbefd754f7d51d
IliasMaz <162236667+IliasMaz@users.noreply.github.com>
2025-11-08 23:11:11 +0200

    app bar with responsiveness through pointer - changed color in the home/page to green

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import { RouterProvider } from 'react-router-dom';
 import { createRouter } from '@/config/create-router';
+import AppBar from './components/AppBar/AppBar';
 
 function App() {
-  return <RouterProvider router={createRouter()} />;
+  <AppBar />;
+  return (
+    <>
+      <AppBar />
+      <RouterProvider router={createRouter()} />
+    </>
+  );
 }
 
 export default App;
