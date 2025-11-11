eb29bc8365d79c61d02aa55d349b50204d551ba1
tBaronDar <thedarelis@gmail.com>
2025-11-11 19:55:57 +0200

    first commit

---
+++ src/config/store.jsx
@@ -0,0 +1,7 @@
+// import redux from 'redux';
+// import thunk from 'redux-thunk';
+// import rootReducer from '../reducers';
+
+// const store = redux.createStore(rootReducer, redux.applyMiddleware(thunk));
+
+// export default store;
