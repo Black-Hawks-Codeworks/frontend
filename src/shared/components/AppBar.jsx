0b3872b0dd42145fc734c8731b9f9c4401523753
tBaronDar <thedarelis@gmail.com>
2025-11-25 14:54:20 +0200

    edited vite.config for app to be accessible from cloudflare tunnel

--- src/shared/components/AppBar.jsx
+++ src/shared/components/AppBar.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import '../../styles/AppBar.css';
 import Icon from '@/shared/icon';
+
 const AppBar = () => {
   return (
     <header className='appbar'>
-
       <div className='appbar-logo actions'>
         <Icon name='Layers' size='md' />
         Electronics
@@ -14,7 +14,6 @@ const AppBar = () => {
         <a href='#home'>Home</a>
         <a href='#parcel'>Your parcel</a>
         <a href='#contact'>Contact</a>
-        
       </nav>
 
       <div className='appbar-actions'>
