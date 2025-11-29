af4a76924c625367421d4b827bec05adac25c8ee
tBaronDar <thedarelis@gmail.com>
2025-11-29 12:17:48 +0200

    css fixes, more comments

--- src/modules/client-dashboard/components/client-dashboard-controls.jsx
+++ src/modules/client-dashboard/components/client-dashboard-controls.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 function ClientDashboardControls() {
   return (
     <div className={styles.container}>
+      {/* otan kano navigate mesa sto domain(dld den feugo apo tin selida mas) hreisimopoio to Link apo react-router-dom, ohi to <a></a> */}
       <Link to='/' className='btn-contained'>
         Back(?)
       </Link>
