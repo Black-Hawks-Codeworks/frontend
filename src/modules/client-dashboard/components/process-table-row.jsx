e9808c91751beee8bfcd2e2ebdd0421c7bc4221a
tBaronDar <thedarelis@gmail.com>
2025-11-29 11:17:12 +0200

    added format date util, css edits

--- src/modules/client-dashboard/components/process-table-row.jsx
+++ src/modules/client-dashboard/components/process-table-row.jsx
@@ -1,14 +1,20 @@
+import styles from './process-table-row.module.css';
+
+function formatDate(date) {
+  return new Date(date).toLocaleDateString('el-GR', { year: 'numeric', month: 'long', day: 'numeric' });
+}
+
 function ProcessTableRow(props) {
   const { process } = props;
   return (
-    <div>
-      <span>{process.processId}</span>
-      <span>{process.cost}</span>
-      <span>{process.name}</span>
-      <span>{process.description}</span>
-      <span>{process.status}</span>
-      <span>{process.createdAt}</span>
-      <span>{process.updatedAt}</span>
+    <div className={styles.container}>
+      <span className={styles.cell}>{process.processId}</span>
+      <span className={styles.cell}>{`${process.cost} €`}</span>
+      <span className={styles.cell}>{process.name}</span>
+      <span className={styles.cell}>{process.description}</span>
+      <span className={`${styles[process.status.toLowerCase()]} ${styles.cell}`}>{process.status}</span>
+      <span className={styles.cell}>{formatDate(process.createdAt)}</span>
+      <span className={styles.cell}>{formatDate(process.updatedAt)}</span>
     </div>
   );
 }
