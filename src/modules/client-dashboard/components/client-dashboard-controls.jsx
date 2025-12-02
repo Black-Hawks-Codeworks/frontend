f9de20c967740eb6a9be9f752f08d5f33ad7e6e9
IliasMaz <162236667+IliasMaz@users.noreply.github.com>
2025-12-02 15:06:53 +0200

    client dashboard - added new btn routed to create new request, wrapped the 2 btns in a new div, added 'Client Dashboad' header for extra ux , fixed margins & gap between tha bts div and h1

--- src/modules/client-dashboard/components/client-dashboard-controls.jsx
+++ src/modules/client-dashboard/components/client-dashboard-controls.jsx
@@ -4,10 +4,16 @@ import { Link } from 'react-router-dom';
 function ClientDashboardControls() {
   return (
     <div className={styles.container}>
+      <h1 className='header-lg text-color-grey-dark'>Client Dashboard</h1>
       {/* otan kano navigate mesa sto domain(dld den feugo apo tin selida mas) hreisimopoio to Link apo react-router-dom, ohi to <a></a> */}
-      <Link to='/' className='btn-contained'>
-        Back(?)
-      </Link>
+      <div className={styles.secondContainer}>
+        <Link to='/' className='btn-contained'>
+          Back(?)
+        </Link>
+        <Link to='/client-page/requests/new_request' className='btn-contained secondButton'>
+          Create New
+        </Link>
+      </div>
     </div>
   );
 }
--- src/modules/client-dashboard/components/process-table.jsx
+++ src/modules/client-dashboard/components/process-table.jsx
@@ -15,6 +15,7 @@ function ProcessTable(props) {
           <span>Status</span>
           <span>Created At</span>
           <span>Updated At</span>
+          
         </div>
         {data.map((process) => (
           <ProcessTableRow key={process.id} process={process} />
