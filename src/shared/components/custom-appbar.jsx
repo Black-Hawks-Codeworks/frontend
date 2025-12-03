ee4dcc6c5813a56edd52dcb17934969882fb3c22
tBaronDar <thedarelis@gmail.com>
2025-12-03 18:44:24 +0200

    conditionaly render appBar links

--- src/shared/components/custom-appbar.jsx
+++ src/shared/components/custom-appbar.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Icon from '@/shared/icon';
-import styles from './custom-appbar.module.css';
 import { useAppSelector, useAppDispatch } from '@/config/store';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import { setUser } from '@/config/reducers/auth.reducer';
+
+import styles from './custom-appbar.module.css';
 
 const AppBar = () => {
   const user = useAppSelector((state) => state.auth?.user);
+  const userRole = user?.role;
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
@@ -22,11 +24,32 @@ const AppBar = () => {
         <p className='header-sm text-color-grey-dark'>Electronics</p>
       </div>
 
-      <nav className={styles.appbarNav}>
-        <a href='#home'>Home</a>
-        <a href='#parcel'>Your parcel</a>
-        <a href='#contact'>Contact</a>
-      </nav>
+      {userRole === 'client' ? (
+        <nav className={styles.appbarNav}>
+          <Link to='/client-dashboard'>Dashboard</Link>
+          <Link to='/create-repair-request'>Your parcel</Link>
+          <Link to='/create-return-request'>Contact</Link>
+          <Link to='/about'>About</Link>
+        </nav>
+      ) : null}
+      {userRole === 'employee' ? (
+        <nav className={styles.appbarNav}>
+          <Link to='/employee-dashboard'>Dashboard</Link>
+          <Link to='/about'>About</Link>
+        </nav>
+      ) : null}
+      {userRole === 'technician' ? (
+        <nav className={styles.appbarNav}>
+          <Link to='/technician-dashboard'>Dashboard</Link>
+          <Link to='/about'>About</Link>
+        </nav>
+      ) : null}
+      {userRole === 'manager' ? (
+        <nav className={styles.appbarNav}>
+          <Link to='/manager-dashboard'>Dashboard</Link>
+          <Link to='/about'>About</Link>
+        </nav>
+      ) : null}
 
       <div className={styles.appbarActions}>{user ? <button onClick={handleLogout}>Logout</button> : null}</div>
     </header>
