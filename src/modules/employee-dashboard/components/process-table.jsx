2b988bbfa9e0fdb6b0bf9760f22950a41ed82f3a
IliasMaz <162236667+IliasMaz@users.noreply.github.com>
2025-12-05 12:05:43 +0200

    process table & process table row with props and .map & mock data

---
+++ src/modules/employee-dashboard/components/process-table.jsx
@@ -0,0 +1,31 @@
+// process-table.jsx
+import styles from './process-table.module.css';
+import ProcessTableRow from './process-table-row';
+
+export default function ProcessTable(props) {
+  const { data } = props;
+
+  return (
+    <div className={`${styles.processTableContainer} card-elevation-1`}>
+      <div className={styles.processTable}>
+        <div className={styles.processTableHeader}>
+          <span>Process ID</span>
+          <span>Product</span>
+          <span>Customer</span>
+          <span>Customer Id</span>
+          <span>Issue</span>
+          <span>Waiting Time</span>
+          <span>Status</span>
+          <span>Assigned To</span>
+          <span>Created At</span>
+          <span>Expected Cost</span>
+          <span>Actions</span>
+        </div>
+
+        {data.map((process) => (
+          <ProcessTableRow key={process.processId} process={process} />
+        ))}
+      </div>
+    </div>
+  );
+}
