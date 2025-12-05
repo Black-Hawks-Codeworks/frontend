62fac89f1174324647c37c663e01d59960434d82
Dimitrios <dplastiras@icloud.com>
2025-12-05 12:00:08 +0200

    Landing Page final touches

--- src/modules/landing/page.jsx
+++ src/modules/landing/page.jsx
@@ -20,30 +20,16 @@ export default function LandingPage() {
       </div>
       <div className={styles.containerbottom}>
         <div className={styles.text2Container}>
-          <h2 className='header-md'>About Us</h2>
-          <p className={`body-lg ${styles['card-elevation-4']}`}>
-            Electronics is a modern, customer‑focused retailer of consumer technology with five physical stores and a
-            thriving e‑commerce channel. From smartphones and laptops to home entertainment and smart appliances, we
-            help customers choose, purchase, and enjoy the right products with expert guidance and competitive prices.
-            Beyond sales, we stand by every purchase with reliable after‑sales support, including streamlined returns
-            and professional repairs through our network of certified service partners. Our mission is to make
-            technology simple, accessible, and dependable—online and in store—backed by transparent service, fast
-            turnaround times, and a commitment to long‑term customer satisfaction.
-          </p>
+          <h2 className='header-xl'>5</h2>
+          <p className='body-xl'> Physical Stores across Greece</p>
         </div>
-        <div>
-          <h2 className='header-md'> About the portal</h2>
-          <p className={`body-lg ${styles['card-elevation-4']}`}>
-            Track and manage your returns and repairs in one place. With our portal, you can start a request in minutes,
-            get instant RMA and shipping labels,see real‑time status updates, approve estimates securely, and chat with
-            support when needed. Enjoy faster turnaround, fewer phone calls, and full transparency from drop‑off to
-            delivery so you always know what’s happening with your device.
-          </p>
-          <h2 className='header-md'>Contact Us</h2>
-          <p className={`body-lg ${styles['card-elevation-4']}`}>
-            For support or inquiries about the portal, please reach out to our customer service team at{' '}
-            <a href='mailto:support@electronics.com'> support@electronics.com</a>{' '}
-          </p>
+        <div className={styles.text2Container}>
+          <h2 className='header-xl'> 20000+</h2>
+          <p className='body-xl'>Unique product codes for electronic items.</p>
+        </div>
+        <div className={styles.text2Container}>
+          <h2 className='header-xl'>15+</h2>
+          <p className='body-xl'>Years of presence.</p>
         </div>
       </div>
     </div>
