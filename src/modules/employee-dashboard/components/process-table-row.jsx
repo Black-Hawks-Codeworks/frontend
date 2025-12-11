f856eba8e2f64479d83d779199e3c6f53ea9f374
Dimitrios <dplastiras@icloud.com>
2025-12-11 23:27:16 +0200

    Fix Employee Dashboard

--- src/modules/employee-dashboard/components/process-table-row.jsx
+++ src/modules/employee-dashboard/components/process-table-row.jsx
@@ -12,20 +12,19 @@ export default function ProcessTableRow(props) {
   return (
     <div className={styles.container}>
       <span className={styles.cell}>{process.processId}</span>
-      <span className={styles.cell}>{process.product}</span>
-      <span className={styles.cell}>{process.customerId}</span>
-      <span className={styles.cell}>{process.issue}</span>
-      <span className={`${styles[process.priority.trim().toLowerCase().replace(/\s+/g, '')]} ${styles.cell}`}>
-        {process.priority}
-      </span>
-      <span className={styles.cell}>{process.waitingTime}</span>
       <span className={`${styles[process.status.trim().toLowerCase().replace(/\s+/g, '')]} ${styles.cell}`}>
         {process.status}
       </span>
+      <span className={styles.cell}>{process.customerId}</span>
+      <span className={styles.cell}>{process.customer}</span>
+      <span className={styles.cell}>###</span>
+      <span className={styles.cell}>{process.product}</span>
+      <span className={styles.cell}>{process.issue}</span>
+      <span className={styles.cell}>###</span>
       <span className={styles.cell}>{process.assignedTo}</span>
-      <span className={styles.cell}>{process.createdAt}</span>
       <span className={styles.cell}>{`${process.expectedCost}€`}</span>
       <span className={styles.cell}>
+        <span className={styles.cell}>{process.createdAt}</span>
         {/* to span einai gia keimeno div gia koutaki */}
         <div className={styles.cellIcons}>
           <Icon name='Bell' size='md' />
