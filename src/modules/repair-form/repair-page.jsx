69eb2480c482865520bc568a8555af4130cd0bc2
nikoleta28a <esd25002@uom.edu.gr>
2025-12-14 20:28:08 +0200

    repair form redesign

--- src/modules/repair-form/repair-page.jsx
+++ src/modules/repair-form/repair-page.jsx
@@ -5,18 +5,33 @@ import Icon from '@/shared/icon';
 export default function RepairFormPage() {
   return (
     <div className={styles.page}>
-      <CreateForm title='Repair Form' showProblemDescription={true} />
-      <div className={`${styles.infoBox} card-elevation-3`}>
-        <div className={styles.infoHeader}>
-          <Icon name='InfoSquare' size='lg' />
-          <p className='header-md'>INFO</p>
+      <div className={`${styles.container} card-elevation`}>
+        <CreateForm title='Repair Form' showProblemDescription={true} />
+        <div className={styles.infoBox}>
+          <div className={styles.infoHeader}>
+            <Icon name='InfoSquare' size='lg' />
+            <p className='header-md'>INFO</p>
+          </div>
+          <ul className='body-xl'>
+            <li>
+              <Icon name='Edit2' size='md' />
+              <b>Fill</b> in all required fields (marked with *)
+            </li>
+            <li>
+              <Icon name='Image1' size='md' />
+              <b>Attach</b> clear photos of the issue if available.
+            </li>
+            <li>
+              <Icon name='Send' size='md' />
+              <b>Submit</b> your request and wait for confirmation email.
+            </li>
+            <li>
+              <Icon name='Search1' size='md' />
+              <b>Track</b>
+              {' your request in the "Your Parcel" section.'}
+            </li>
+          </ul>
         </div>
-        <ol className='body-xl'>
-          <li>Fill in all required fields (marked with *)</li>
-          <li>Attach clear photos of the issue if available.</li>
-          <li>Submit your request and wait for confirmation email.</li>
-          <li>{'Track your request in the "Your Parcel" section.'}</li>
-        </ol>
       </div>
     </div>
   );
--- src/shared/components/forms/create-form.jsx
+++ src/shared/components/forms/create-form.jsx
@@ -2,22 +2,18 @@ import FormField from './form-field';
 import styles from './form.module.css';
 export default function CreateForm({ title, showProblemDescription = false }) {
   return (
-    <div>
-      <form className={`${styles.form}  card-elevation-3`}>
-        <p className='header-lg text-color-primary-shade-2 '>{title} </p>
-        <div className={styles.container}>
-          <FormField label='Product Type*' id='product-type' type='select' />
-          <FormField label='Serial number*' id='serial-number' type='text' placeholder='03npk' />
-          <FormField label='Purchase date*' id='purchase-date' type='date' />
-          {showProblemDescription && (
-            <FormField label='Problem Description*' id='problem-description' type='textarea' />
-          )}
-          <FormField label='Upload images (optional)' id='upload-images' type='file' multiple='image/*' />
-          <button type='submit' className={`${styles.smallBtn} btn-contained`}>
-            SUBMIT
-          </button>
-        </div>
-      </form>
-    </div>
+    <form className={styles.form}>
+      <p className='header-lg text-color-primary-shade-2 '>{title} </p>
+      <div className={styles.container}>
+        <FormField label='Product Type*' id='product-type' type='select' />
+        <FormField label='Serial number*' id='serial-number' type='text' placeholder='03npk' />
+        <FormField label='Purchase date*' id='purchase-date' type='date' />
+        {showProblemDescription && <FormField label='Problem Description*' id='problem-description' type='textarea' />}
+        <FormField label='Upload images (optional)' id='upload-images' type='file' multiple='image/*' />
+        <button type='submit' className={`${styles.smallBtn} btn-contained`}>
+          SUBMIT
+        </button>
+      </div>
+    </form>
   );
 }
