a62ec8a344484d70eb20ed7d4521f14abb2a4730
IliasMaz <162236667+IliasMaz@users.noreply.github.com>
2025-12-16 12:40:00 +0200

    total line charts = 3 . 1st total requests yearly, 2nd active tickets this month , 3rd total resolved yearly

--- src/modules/manager-dashboard/components/HomeRightBar/home-right-bar.jsx
+++ src/modules/manager-dashboard/components/HomeRightBar/home-right-bar.jsx
@@ -3,6 +3,12 @@ import Navbar from '../Navbar/navbar';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 
+const data2 = [
+  { name: 'Dec 1–10', rt: 3100, rp: 1850 },
+  { name: 'Dec 11–20', rt: 2650, rp: 1600 },
+  { name: 'Dec 21–31', rt: 2040, rp: 1250 },
+];
+
 const data = [
   {
     name: 'Jan',
@@ -48,6 +54,21 @@ const data = [
   },
 ];
 
+export const data3 = [
+  { name: 'Jan', rt: 360, rp: 260 },
+  { name: 'Feb', rt: 320, rp: 240 },
+  { name: 'Mar', rt: 310, rp: 230 },
+  { name: 'Apr', rt: 300, rp: 220 },
+  { name: 'May', rt: 370, rp: 290 },
+  { name: 'Jun', rt: 310, rp: 230 },
+  { name: 'Jul', rt: 320, rp: 250 },
+  { name: 'Aug', rt: 330, rp: 260 },
+  { name: 'Sep', rt: 300, rp: 230 },
+  { name: 'Oct', rt: 310, rp: 240 },
+  { name: 'Nov', rt: 340, rp: 260 },
+  { name: 'Dec', rt: 370, rp: 290 },
+];
+
 export default function HomeRightBar() {
   return (
     <div className={styles.mainHomeRightBar}>
@@ -55,11 +76,12 @@ export default function HomeRightBar() {
 
       <div>
         <div className={styles.ItemContainer}>
+          {/* Total Requests Line Chart (1) */}
           <div className={styles.ItemContainer1}>
             <div>
-              <p className={`${styles.taskProgress} text-color-black `}>Total Progress</p>
-              <p className={`${styles.taskCounter}  text-color-primary-shade-4 `}>8015</p>
-              <p className={` ${styles.currentMonth1} text-color-grey-dark`}>Current Month</p>
+              <p className={`${styles.taskProgress} text-color-grey `}>Total Requests</p>
+              <p className={`${styles.taskCounter}  text-color-primary-shade-4 `}>7790</p>
+              <p className={` ${styles.currentMonth1} text-color-grey header-sm `}>2025</p>
             </div>
             <div>
               <LineChart
@@ -77,13 +99,71 @@ export default function HomeRightBar() {
                 <YAxis width='auto' />
                 <Tooltip />
                 <Legend />
-                <Line type='monotone' dataKey='rt' stroke='#8884d8' activeDot={{ r: 8 }} />
+                <Line type='monotone' dataKey='rt' stroke='#2194f3' activeDot={{ r: 8 }} />
                 <Line type='monotone' dataKey='rp' stroke='#82ca9d' />
               </LineChart>
             </div>
           </div>
-          <div className={styles.ItemContainer1}>2</div>
-          <div className={styles.ItemContainer1}>3</div>
+
+          {/* Active Tickets (2) */}
+
+          <div className={styles.ItemContainer1}>
+            <div>
+              <p className={`${styles.taskProgress2} text-color-grey `}>Active Tickets</p>
+              <p className={`${styles.taskCounter2}  text-color-primary-shade-4 `}>4800</p>
+              <p className={` ${styles.currentMonth2} text-color-grey header-sm `}>Current Month</p>
+            </div>
+            <div>
+              <LineChart
+                style={{ width: '100%', maxWidth: '700px', height: '100%', maxHeight: '70vh', aspectRatio: 1.618 }}
+                responsive
+                data={data2}
+                margin={{
+                  top: 5,
+                  right: 0,
+                  left: 0,
+                  bottom: 5,
+                }}>
+                <CartesianGrid strokeDasharray='3 3' />
+                <XAxis dataKey='name' />
+                <YAxis width='auto' />
+                <Tooltip />
+                <Legend />
+                <Line type='monotone' dataKey='rt' stroke='#2194f3' activeDot={{ r: 8 }} />
+                <Line type='monotone' dataKey='rp' stroke='#82ca9d' />
+              </LineChart>
+            </div>
+          </div>
+
+          {/* Monthly KPIS*/}
+          {/* RESOLVED REPAIRS & REPAIRS */}
+          <div className={styles.ItemContainer1}>
+            <div>
+              <p className={`${styles.taskProgress} text-color-grey `}>Total Resolved</p>
+              <p className={`${styles.taskCounter3}  text-color-primary-shade-4 `}>6940</p>
+              <p className={` ${styles.currentMonth1} text-color-grey header-sm `}>2025</p>
+            </div>
+            <div>
+              <LineChart
+                style={{ width: '100%', maxWidth: '700px', height: '100%', maxHeight: '70vh', aspectRatio: 1.618 }}
+                responsive
+                data={data3}
+                margin={{
+                  top: 5,
+                  right: 0,
+                  left: 0,
+                  bottom: 5,
+                }}>
+                <CartesianGrid strokeDasharray='3 3' />
+                <XAxis dataKey='name' />
+                <YAxis width='auto' />
+                <Tooltip />
+                <Legend />
+                <Line type='monotone' dataKey='rt' stroke='#fbc02d' activeDot={{ r: 8 }} />
+                <Line type='monotone' dataKey='rp' stroke='#82ca9d' />
+              </LineChart>
+            </div>
+          </div>
         </div>
       </div>
 
