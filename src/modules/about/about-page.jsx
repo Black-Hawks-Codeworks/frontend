17db3698751baa84e76374153d2ce2b352595815
Adamantios Litzerinos <adamlitzerinos@gmail.com>
2025-12-17 01:16:17 +0200

    KAROUZELAKI kai sousou

--- src/modules/about/about-page.jsx
+++ src/modules/about/about-page.jsx
@@ -1,7 +1,31 @@
+// AboutPage.jsx (or .js)
 import './about-page.css';
 import Carousel from './carousel';
 import { personaldata } from './personal_info';
-import img1 from '../../assets/Photos/cmnd.jpg';
+
+// Adjust the path (../ or ../../) according to where AboutPage.jsx is located
+// Assuming AboutPage.jsx is two levels below src (e.g. src/components/pages/AboutPage.jsx):
+// If it's only one level (e.g. src/pages/AboutPage.jsx), change ../../ to ../
+
+// Import ALL photos (note: photoX.jpeg)
+import photo1 from '../../assets/Photos/photo1.jpeg';
+import photo2 from '../../assets/Photos/photo2.jpeg';
+
+import photo4 from '../../assets/Photos/photo4.jpeg';
+import photo5 from '../../assets/Photos/photo5.jpeg';
+import photo6 from '../../assets/Photos/photo6.jpeg';
+import photo7 from '../../assets/Photos/photo7.jpeg';
+
+// Map each id to the corresponding imported image
+const photosById = {
+  1: photo1,
+  2: photo2,
+
+  4: photo4,
+  5: photo5,
+  6: photo6,
+  7: photo7,
+};
 
 export default function AboutPage() {
   return (
@@ -16,7 +40,7 @@ export default function AboutPage() {
           {personaldata.map((personal) => (
             <div className='pcard' key={personal.id}>
               <div className='personalPhoto'>
-                <img src={img1} alt={personal.name} />
+                <img src={photosById[personal.id]} alt={personal.name} />
               </div>
               <div className='personalText'>
                 <div className='personalName'>{personal.name}</div>
