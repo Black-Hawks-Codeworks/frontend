32e3f53f21fe891f4f5332dc107192eefa137527
nikoleta28a <esd25002@uom.edu.gr>
2025-12-16 14:01:21 +0200

    edit form functions

--- src/shared/forms/create-form.jsx
+++ src/shared/forms/create-form.jsx
@@ -1,18 +1,78 @@
 import FormField from './form-field';
 import styles from './form.module.css';
+import { useState } from 'react';
+
 export default function CreateForm({ title, showProblemDescription = false }) {
+  const [inputs, setInputs] = useState({
+    productType: '',
+    serialNumber: '',
+    purchaseDate: '',
+    problemDescription: '',
+    uploadImages: '',
+  });
+
+  const handleChange = (e) => {
+    const target = e.target;
+    const value = target.type === 'file' ? target.files : target.value;
+    const name = target.name;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('submitted');
+    console.log(inputs);
+    alert('submitted');
+  };
+
   return (
     <div>
-      <form className={`${styles.form}  card-elevation-3`}>
+      <form onSubmit={handleSubmit} className={`${styles.form}  card-elevation-3`}>
         <p className='header-lg text-color-primary-shade-2 '>{title} </p>
         <div className={styles.container}>
-          <FormField label='Product Type*' id='product-type' type='select' />
-          <FormField label='Serial number*' id='serial-number' type='text' placeholder='03npk' />
-          <FormField label='Purchase date*' id='purchase-date' type='date' />
+          <FormField
+            label='Product Type*'
+            id='productType'
+            type='select'
+            value={inputs.productType}
+            onChange={handleChange}
+            required={true}
+          />
+          <FormField
+            label='Serial number*'
+            id='serialNumber'
+            type='text'
+            placeholder='03npk'
+            value={inputs.serialNumber}
+            onChange={handleChange}
+            required={true}
+          />
+          <FormField
+            label='Purchase date*'
+            id='purchaseDate'
+            type='date'
+            value={inputs.purchaseDate}
+            onChange={handleChange}
+            required={true}
+          />
           {showProblemDescription && (
-            <FormField label='Problem Description*' id='problem-description' type='textarea' />
+            <FormField
+              label='Problem Description*'
+              id='problemDescription'
+              type='textarea'
+              value={inputs.problemDescription}
+              onChange={handleChange}
+              required={true}
+            />
           )}
-          <FormField label='Upload images (optional)' id='upload-images' type='file' multiple='image/*' />
+          <FormField
+            label='Upload images (optional)'
+            id='uploadImages'
+            type='file'
+            multiple={true}
+            onChange={handleChange}
+            required={false}
+          />
           <button type='submit' className={`${styles.smallBtn} btn-contained`}>
             SUBMIT
           </button>
--- src/shared/forms/form-field.jsx
+++ src/shared/forms/form-field.jsx
@@ -1,14 +1,15 @@
 import styles from './form.module.css';
 import { category } from '../../modules/repair-form/category';
 
-export default function FormField({ id, label, type, placeholder, multiple }) {
+export default function FormField({ id, label, type, placeholder, multiple, onChange, required }) {
   return (
-    // <div className={`${styles.row} body-xl`}>
     <div className={styles.row}>
       <label htmlFor={id}>{label}</label>
-      {type === 'textarea' && <textarea id={id} name={id} placeholder={placeholder} required />}
+      {type === 'textarea' && (
+        <textarea id={id} name={id} placeholder={placeholder} onChange={onChange} required={required} />
+      )}
       {type === 'select' && (
-        <select id={id} name={id} required>
+        <select id={id} name={id} onChange={onChange} required={required}>
           <option value=''>Select a product</option>
           {category.map((item) => (
             <option key={item.categoryId} value={item.category}>
@@ -21,11 +22,11 @@ export default function FormField({ id, label, type, placeholder, multiple }) {
         <input
           id={id}
           name={id}
+          onChange={onChange}
           type={type}
           placeholder={placeholder}
-          multiple
-          accept={multiple}
-          required={type === 'file' ? false : true}
+          multiple={multiple}
+          required={required}
         />
       )}
     </div>
