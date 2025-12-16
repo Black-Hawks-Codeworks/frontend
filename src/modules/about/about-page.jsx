e12cce82d82e5349ba41c27978a03739441fbe14
Dimitrios <dplastiras@icloud.com>
2025-12-17 00:34:24 +0200

    Creation of the Personal Cards

--- src/modules/about/about-page.jsx
+++ src/modules/about/about-page.jsx
@@ -1,18 +1,34 @@
 import './about-page.css';
 import Carousel from './carousel';
+import { personaldata } from './personal_info';
+import img1 from '../../assets/Photos/cmnd.jpg';
 
 export default function AboutPage() {
   return (
-    <div className='container'>
-      <div className='a1'>
-        <Carousel />
-      </div>
-      <div className='b1'>
-        <p> Info about the team</p>
-        <div> </div>
-      </div>
-      <div className='c1'>
-        <p> Project Info</p>
+    <div className='title'>
+      <h1>About us</h1>
+      <div className='container'>
+        <div className='carousel'>
+          <Carousel />
+        </div>
+
+        <div className='personalInfoArea'>
+          {personaldata.map((personal) => (
+            <div className='pcard' key={personal.id}>
+              <div className='personalPhoto'>
+                <img src={img1} alt={personal.name} />
+              </div>
+              <div className='personalText'>
+                <div className='personalName'>{personal.name}</div>
+                <div className='personalBio'>{personal.bio}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className='c1'>
+          <p>Project Info</p>
+        </div>
       </div>
     </div>
   );
---
+++ src/modules/about/personal_info.js
@@ -0,0 +1,45 @@
+export const personaldata = [
+  {
+    id: 1,
+    name: 'Amanatidou Nikoleta',
+    bio: 'A higly motivated professional. Nikoleta already holds a Bachelors degree BLALALALALALALALALALAALA',
+    linkedin: '',
+  },
+
+  {
+    id: 2,
+    name: 'Darelis Themistoklis (Themis)',
+    bio: '',
+    linkedin: '',
+  },
+  {
+    id: 3,
+    name: 'Zarkada Vasiliki',
+    bio: '',
+    linkedin: '',
+  },
+  {
+    id: 4,
+    name: 'Karanikas Stylianos',
+    bio: '',
+    linkedin: '',
+  },
+  {
+    id: 5,
+    name: 'Litzerinos Adamantios (Danis)',
+    bio: '',
+    linkedin: '',
+  },
+  {
+    id: 6,
+    name: 'Mazarakis Ilias',
+    bio: '',
+    linkedin: '',
+  },
+  {
+    id: 7,
+    name: 'Plastiras Dimitrios',
+    bio: '',
+    linkedin: '',
+  },
+];
