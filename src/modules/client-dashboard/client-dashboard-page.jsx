597c76bc3d1d830a77bdb714f8b5ac8b9ef651ea
IliasMaz <162236667+IliasMaz@users.noreply.github.com>
2025-12-28 23:35:01 +0200

    created process-cards view & added a terminary at client dashboard so by toggling the right button it shows the right component(table view or cards view)

--- src/modules/client-dashboard/client-dashboard-page.jsx
+++ src/modules/client-dashboard/client-dashboard-page.jsx
@@ -1,13 +1,20 @@
 import ProcessTable from './components/process-table';
-import { data } from './dummy-data';
+import { data } from '../data/dummy-data-client';
 import ClientDashboardControls from './components/client-dashboard-controls';
 import styles from './client-dashboard-page.module.css';
+import { useState } from 'react';
+import ProcessCards from './components/process-cards';
 
 function ClientDashboardPage() {
+
+  const[viewMode,setViewMode]=useState('table');
+
+
+
   return (
     <div className={styles.pageContainer}>
-      <ClientDashboardControls />
-      <ProcessTable data={data} />
+      <ClientDashboardControls viewMode={viewMode} setViewMode={setViewMode} />
+      {viewMode==='table' ? <ProcessTable data={data}/> : <ProcessCards data={data}/>}
     </div>
   );
 }
--- src/modules/client-dashboard/components/client-dashboard-controls.jsx
+++ src/modules/client-dashboard/components/client-dashboard-controls.jsx
@@ -1,17 +1,35 @@
 import styles from './client-dashboard-controls.module.css';
 import { Link } from 'react-router-dom';
 
-function ClientDashboardControls() {
+function ClientDashboardControls({ viewMode, setViewMode }) {
+  function handleTableClick(e) {
+    e.preventDefault();
+    setViewMode('table');
+  }
+
+  function handleCardsClick(e) {
+    e.preventDefault();
+    setViewMode('cards');
+  }
+
   return (
     <div className={styles.container}>
       <h1 className='header-lg text-color-grey-dark'>Client Dashboard</h1>
       {/* otan kano navigate mesa sto domain(dld den feugo apo tin selida mas) hreisimopoio to Link apo react-router-dom, ohi to <a></a> */}
       <div className={styles.secondContainer}>
-        <Link to='/' className='btn-contained'>
-          Back(?)
+        <Link
+          to='#'
+          className='btn-contained'
+          onClick={handleTableClick}
+          aria-current={viewMode === 'table' ? 'page' : undefined}>
+          Table View
         </Link>
-        <Link to='/client-page/requests/new_request' className='btn-contained secondButton'>
-          Create New
+        <Link
+          to='#'
+          className='btn-contained secondButton'
+          onClick={handleCardsClick}
+          aria-current={viewMode === 'cards' ? 'page' : undefined}>
+          Card View
         </Link>
       </div>
     </div>
