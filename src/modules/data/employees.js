61367586eac20bd4cd85e98de962992cb756f46e
Themis Darelis <thedarelis@gmail.com>
2025-12-30 16:19:23 +0200

    created skeleton for json db

---
+++ src/modules/data/employees.js
@@ -0,0 +1,12 @@
+export const employees = [
+  {
+    id: 1,
+    username: 'nikol',
+    password: '123456',
+    name: 'Nikol Amanatidou',
+    email: 'nikol.amanatidou@example.com',
+    phone: '6977889900',
+    address: '123 Main St, Anytown, USA',
+    role: 'employee',
+  },
+];
---
+++ src/modules/data/managers.js
@@ -0,0 +1,12 @@
+export const managers = [
+  {
+    id: 1,
+    name: 'Vasiliki Zarladaki',
+    username: 'vasiliki',
+    password: '9dahtila',
+    email: 'vasiliki.zarladaki@example.com',
+    phone: '6977889900',
+    address: '123 Main St, Anytown, USA',
+    role: 'manager',
+  },
+];
