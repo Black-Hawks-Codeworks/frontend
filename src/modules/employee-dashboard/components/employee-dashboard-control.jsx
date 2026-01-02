5f62cebade0b31468eaba3dda1a2e7127993aa4b
Dimitrios <dplastiras@icloud.com>
2026-01-03 01:42:36 +0200

    Carousel size adjustment

--- src/modules/employee-dashboard/components/employee-dashboard-control.jsx
+++ src/modules/employee-dashboard/components/employee-dashboard-control.jsx
@@ -6,10 +6,11 @@ function EmployeeDashboardControls(props) {
     <div className={styles.container}>
       <h1 className='header-lg text-color-grey-dark'>Employee Dashboard</h1>
       <div className={styles.secondContainer}>
-        <button onClick={() => setView('table')} className='btn-contained secondButton'>
-          All Tickets
-        </button>
-        
+        {
+          <button onClick={() => setView('table')} className='btn-contained secondButton'>
+            All Tickets
+          </button>
+        }
       </div>
     </div>
   );
