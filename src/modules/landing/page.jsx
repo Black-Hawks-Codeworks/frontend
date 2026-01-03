1d9625a621a8faa75473253446635c01724cd5d6
Themis Darelis <thedarelis@gmail.com>
2026-01-03 15:23:38 +0200

    created hook for landing page, removed users from store

--- src/modules/landing/page.jsx
+++ src/modules/landing/page.jsx
@@ -1,65 +1,17 @@
 import styles from './page.module.css';
 import Login from '@/modules/landing/components/login';
 import Icon from '@/shared/icon';
-import { setUser } from '@/config/reducers/auth.reducer';
-import { Navigate, useNavigate } from 'react-router-dom';
-import { useAppDispatch, useAppSelector, getStore } from '@/config/store';
+import useLanding from '@/modules/landing/hooks/use-landing';
 
 export default function LandingPage() {
-  const navigate = useNavigate();
-  //to dispatch kai to useAppSelector einai oi sinartiseis pou allazoume kai pernoume to store
-  //to store einai to global state tou app, perissotera @/config/store.js
-  //i logiki auti tha metaferthei sto backend kapoia stigmi edo tha ehoume user h null
-  const dispatch = useAppDispatch();
-  const technicians = useAppSelector((state) => state.auth?.technicians);
-  const managers = useAppSelector((state) => state.auth?.managers);
-  const clients = useAppSelector((state) => state.auth?.clients);
-  const employees = useAppSelector((state) => state.auth?.employees);
+  const { authUser } = useLanding();
 
-  const possibleUsers = [...technicians, ...managers, ...clients, ...employees];
-  console.log(possibleUsers);
-  //mehri edo, backend
-
-  //edo an iparhei user feugoume apo to landing page se to dashboard pou antistoixei ston role tou user
-  const currentUser = getStore().getState().auth.user;
-  if (currentUser) {
-    const currentUserRole = currentUser.role;
-    return <Navigate to={`/${currentUserRole}-dashboard`} />;
-  }
-
-  //pernoume ton user apo to backend me async function
-  //to route prepei na eina lathos
-  //gia na doulepsei prepei na trehei to backend
-  async function authUserAsync(username, password) {
-    try {
-      const response = await fetch('/api/auth/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, password }),
-      });
-      const data = await response.json();
-      if (response.ok) {
-        dispatch(setUser(data));
-        const userRole = data.role;
-        navigate(`/${userRole}-dashboard`);
-      } else {
-        dispatch(setUser(null));
-      }
-    } catch (error) {
-      dispatch(setUser(null));
-      console.error('Error:', error);
-    }
-  }
-
-  console.log(authUserAsync);
   return (
     <div className={styles.container}>
       <div className={styles.containertop}>
         <div className={styles.loginContainer}>
           <h2 className='header-md'>Login</h2>
-          <Login onSubmit={authUserAsync} />
+          <Login onSubmit={authUser} />
         </div>
         <div className={styles.textContainer}>
           <Icon name='Layers' size='xl' />
