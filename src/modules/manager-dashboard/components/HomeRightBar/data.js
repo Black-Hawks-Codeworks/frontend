0c0c424758fa16ada9d67f56f970589016c18cb8
IliasMaz <162236667+IliasMaz@users.noreply.github.com>
2026-01-08 15:19:02 +0200

    data fix

---
+++ src/modules/manager-dashboard/components/HomeRightBar/data.js
@@ -0,0 +1,110 @@
+export const data2 = [
+  { name: 'Dec 1–10', rt: 3100, rp: 1850 },
+  { name: 'Dec 11–20', rt: 2650, rp: 1600 },
+  { name: 'Dec 21–31', rt: 2040, rp: 1250 },
+];
+
+export const data = [
+  {
+    name: 'Jan',
+    rt: 4000,
+    rp: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Mar',
+    rt: 3000,
+    rp: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'May',
+    rt: 2000,
+    rp: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Jul',
+    rt: 2780,
+    rp: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Aug',
+    rt: 1890,
+    rp: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Nov',
+    rt: 2390,
+    rp: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Dec',
+    rt: 3490,
+    rp: 4300,
+    amt: 2100,
+  },
+];
+
+export const data3 = [
+  { name: 'Jan', rt: 360, rp: 260 },
+  { name: 'Feb', rt: 320, rp: 240 },
+  { name: 'Mar', rt: 310, rp: 230 },
+  { name: 'Apr', rt: 300, rp: 220 },
+  { name: 'May', rt: 370, rp: 290 },
+  { name: 'Jun', rt: 310, rp: 230 },
+  { name: 'Jul', rt: 320, rp: 250 },
+  { name: 'Aug', rt: 330, rp: 260 },
+  { name: 'Sep', rt: 300, rp: 230 },
+  { name: 'Oct', rt: 310, rp: 240 },
+  { name: 'Nov', rt: 340, rp: 260 },
+  { name: 'Dec', rt: 370, rp: 290 },
+];
+
+export const data4 = [
+  {
+    name: 'Jan',
+    rp: 4000,
+    rt: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Mar',
+    rp: 3000,
+    rt: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'May',
+    rp: 2000,
+    rt: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Jun',
+    rp: 2780,
+    rt: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Aug',
+    rp: 1890,
+    rt: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Nov',
+    rp: 2390,
+    rt: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Dec',
+    rp: 3490,
+    rt: 4300,
+    amt: 2100,
+  },
+];
